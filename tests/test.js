0d1b9b131d0dfa1cadf28aef95a558cfeefb8771
Eric Mill <eric.mill@gsa.gov>
2014-10-19 22:27:46 -0400

    Add ability to resume multipart uploads.
    Adds second optional argument to constructor, `sessionDetails`,
    which can receive an UploadId and set of Parts as recorded from
    a previous session.
    
    Important: the Parts array must be contiguous and start with 1.
    There should be no gaps, as the new part number is calculated from
    the length of the array, not by actually looking at the numbers.
    
    On resume, a 'ready' event will be fired just before the first new
    part begins uploading, with the resumed UploadId.
    
    Tests included.

--- tests/test.js
+++ tests/test.js
@@ -36,10 +36,10 @@ var AWSstub = {
       expect(details.Key).to.be.a('string');
 
       expect(details).to.have.property('UploadId');
-      expect(details.UploadId).to.equal('upload-id');
+      expect(details.UploadId).to.contain('upload-id');
 
       expect(details).to.have.property('PartNumber');
-      expect(details.PartNumber).to.an.integer;
+      expect(details.PartNumber).to.be.an.integer;
 
       if (details.Key == 'upload-fail') {
         callback('Simulated failure from mocked API');
@@ -61,7 +61,7 @@ var AWSstub = {
       expect(details.Key).to.be.a('string');
 
       expect(details).to.have.property('UploadId');
-      expect(details.UploadId).to.equal('upload-id');
+      expect(details.UploadId).to.contain('upload-id');
 
       if (details.Key == 'abort-fail') {
         // Trigger a simulated error when a magic file name is used.
@@ -81,7 +81,7 @@ var AWSstub = {
       expect(details.Key).to.be.a('string');
 
       expect(details).to.have.property('UploadId');
-      expect(details.UploadId).to.equal('upload-id');
+      expect(details.UploadId).to.contain('upload-id');
 
       expect(details).to.have.property('MultipartUpload');
       expect(details.MultipartUpload).to.an.object;
@@ -223,7 +223,16 @@ describe('Piping data into the writable upload stream', function () {
   it('should emit valid part and uploaded events', function (done) {
     var file = fs.createReadStream(process.cwd() + '/tests/test.js');
 
-    var part = false, uploaded = false;
+    var ready = false, part = false, uploaded = false;
+
+    uploadStream.on('ready', function(uploadId) {
+      ready = true;
+
+      expect(uploadId).to.equal('upload-id');
+
+      if (ready & part & uploaded)
+        done();
+    });
 
     uploadStream.on('part', function (details) {
       part = true;
@@ -232,7 +241,7 @@ describe('Piping data into the writable upload stream', function () {
       expect(details.ETag).to.equal('etag');
 
       expect(details).to.have.property('PartNumber');
-      expect(details.PartNumber).to.be.an.integer;
+      expect(details.PartNumber).to.equal(1);
 
       expect(details).to.have.property('receivedSize');
       expect(details.receivedSize).to.be.an.integer;
@@ -240,14 +249,106 @@ describe('Piping data into the writable upload stream', function () {
       expect(details).to.have.property('uploadedSize');
       expect(details.uploadedSize).to.be.an.integer;
 
-      if (part & uploaded)
+      if (ready & part & uploaded)
         done();
     });
 
     uploadStream.on('uploaded', function () {
       uploaded = true;
 
-      if (part & uploaded)
+      if (ready & part & uploaded)
+        done();
+    });
+
+    file.on('open', function () {
+      file.pipe(uploadStream);
+    });
+
+    file.on('error', function () {
+      throw 'Error! Unable to open the file for reading';
+    });
+  });
+});
+
+
+/*
+  Differences from normal creation:
+  * Constructor passes multipartUploadId and part info
+  * 'ready' event fires with given multipartUploadID
+  * First sent part number should start 1 above those given
+
+  ASSUMPTION:
+    Parts are passed in without gaps. Part number is calculated
+    based on array length, not at inspecting given part numbers.
+*/
+function nodescribe() {}; // DEBUG
+describe('Piping data into a resumed upload stream', function () {
+  var uploadStream;
+
+  before(function (done) {
+    uploadStream = new s3Stream.upload({
+      "Bucket": "test-bucket-name",
+      "Key": "test-file-name"
+    }, {
+      // when 'ready' event fires, should have this ID
+      "UploadId": "this-tests-specific-upload-id",
+      "Parts": [
+        {
+          "PartNumber": 1,
+          "ETag": "etag-1"
+        },
+        {
+          "PartNumber": 2,
+          "ETag": "etag-2"
+        }
+      ]
+    });
+
+    uploadStream.on('error', function () {
+      throw "Did not expect to receive an error";
+    });
+
+    done();
+  });
+
+  it('should emit valid part and uploaded events', function (done) {
+    var file = fs.createReadStream(process.cwd() + '/tests/test.js');
+
+    var ready = false, part = false, uploaded = false;
+
+    uploadStream.on('ready', function(uploadId) {
+      ready = true;
+
+      expect(uploadId).to.equal('this-tests-specific-upload-id');
+
+      if (ready & part & uploaded)
+        done();
+    });
+
+    uploadStream.on('part', function (details) {
+      part = true;
+
+      expect(details).to.have.property('ETag');
+      expect(details.ETag).to.equal('etag');
+
+      // part number should be one more than the highest given
+      expect(details).to.have.property('PartNumber');
+      expect(details.PartNumber).to.equal(3);
+
+      expect(details).to.have.property('receivedSize');
+      expect(details.receivedSize).to.be.an.integer;
+
+      expect(details).to.have.property('uploadedSize');
+      expect(details.uploadedSize).to.be.an.integer;
+
+      if (ready & part & uploaded)
+        done();
+    });
+
+    uploadStream.on('uploaded', function () {
+      uploaded = true;
+
+      if (ready & part & uploaded)
         done();
     });
 
